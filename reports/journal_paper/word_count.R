3b8e4083f4d56c3f188c632351dc7dce0bb0ee0e
Mika Braginsky <mika.br@gmail.com>
2018-08-24 17:52:00 -0700

    si streamline

--- reports/journal_paper/word_count.R
+++ reports/journal_paper/word_count.R
@@ -1,21 +1,22 @@
 library(tidyverse)
 
 word_count <- function(filename) {
-  read_file(filename) %>%
+  str <- read_file(filename) %>%
     str_replace_all("\\\n", " ") %>%
     str_replace_all("---.*---", "") %>%
     str_replace_all("```\\{r.*?```", "") %>%
     str_replace_all("`r .*?`", "") %>%
     str_replace_all("\\$.*?\\$", "") %>%
     str_replace_all("@[a-zA-Z0-9]*", "") %>%
+    str_replace_all("\\\\ \\\\", " \\\\") %>%
     str_replace_all("\\\\.*? ", "") %>%
     str_replace_all("#+", "") %>%
-    str_replace_all("\\[.*?\\]", "") %>%
-    str_replace_all("\\(.*?\\)", "") %>%
     str_replace_all("[^A-Za-z ]", "") %>%
     str_replace_all("[ \t]{2,}", " ") %>%
-    str_replace_all("^[ \t]", "") %>%
-    # write_file("aoapred_words.txt")
+    str_replace_all("^[ \t]", "")
+
+  write_file(str, "aoapred_words.txt")
+  str %>%
     str_split(" ") %>%
     .[[1]] %>%
     length()
