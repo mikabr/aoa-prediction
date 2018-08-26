ee79bb0d14c38d53cca4cbf07f172dd0f181019d
Mika Braginsky <mika.br@gmail.com>
2018-08-26 15:35:45 -0700

    virginia trimming, exclude acknowledges and footnotes from word count

--- reports/journal_paper/word_count.R
+++ reports/journal_paper/word_count.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 word_count <- function(filename) {
-  str <- read_file(filename) %>%
+  st <- read_file(filename) %>%
     str_replace_all("\\\n", " ") %>%
     str_replace_all("---.*---", "") %>%
     str_replace_all("```\\{r.*?```", "") %>%
@@ -10,13 +10,15 @@ word_count <- function(filename) {
     str_replace_all("@[a-zA-Z0-9]*", "") %>%
     str_replace_all("\\\\ \\\\", " \\\\") %>%
     str_replace_all("\\\\.*? ", "") %>%
+    str_replace_all("\\^\\[.*?\\]", "") %>%
     str_replace_all("#+", "") %>%
+    str_replace_all("Acknowledgements.*", "") %>%
     str_replace_all("[^A-Za-z ]", "") %>%
     str_replace_all("[ \t]{2,}", " ") %>%
     str_replace_all("^[ \t]", "")
 
-  write_file(str, "aoapred_words.txt")
-  str %>%
+  write_file(st, "aoapred_words.txt")
+  st %>%
     str_split(" ") %>%
     .[[1]] %>%
     length()
