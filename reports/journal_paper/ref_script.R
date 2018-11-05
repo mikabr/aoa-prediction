0ea60f5a2f793705a99bcec3e2460f4d05ab4909
Mika Braginsky <mika.br@gmail.com>
2018-11-04 21:24:10 -0500

    reformat for openmind style, misc appearance tweaks

--- reports/journal_paper/ref_script.R
+++
@@ -1,8 +0,0 @@
-cites <- read_file("aoapred.Rmd") %>% str_extract_all("@[a-z0-9]+") %>%
-  .[[1]] %>% str_sub(2, str_length(.))
-
-bibs <- read_file("aoapred.bib") %>% str_match_all("@.*\\{(.*),") %>%
-  .[[1]] %>% .[,2]
-
-unused <- setdiff(bibs, cites) %>% sort()
-missing <- setdiff(cites, bibs) %>% sort()
